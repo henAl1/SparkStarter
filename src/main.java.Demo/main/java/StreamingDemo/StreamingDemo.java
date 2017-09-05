b4e9d52eac5f87e3afbedb7c2e5171ec06cec7a0
henAlene <henokster@gmail.com>
2017-09-04 23:21:35 -0700

    starter for batch and streaming spark code

---
+++ src/main.java.Demo/main/java/StreamingDemo/StreamingDemo.java
@@ -0,0 +1,18 @@
+package main.java.StreamingDemo;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+/**
+ * Created by henok on 9/4/2017.
+ */
+public class StreamingDemo {
+
+    public static void main(String ...args) {
+        String master = "local[*]";
+        SparkConf conf = new SparkConf().setAppName(StreamingDemo.class.getName()).setMaster(master);
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+    }
+
+}
---
+++ src/main.java.Demo/main/java/batchDemo/BatchDemo.java
@@ -0,0 +1,33 @@
+package main.java.batchDemo;
+
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by henok on 9/4/2017.
+ */
+public class BatchDemo {
+
+
+    public static void main(String ...args) {
+        String master = "local[*]";
+        SparkConf conf = new SparkConf().setAppName(BatchDemo.class.getName()).setMaster(master);
+        JavaSparkContext context = new JavaSparkContext(conf);
+
+
+        context.textFile("test")
+                .flatMap(text -> Arrays.asList(text.split(" ")).iterator())
+                .mapToPair(word ->new Tuple2<>(word,1))
+                .reduceByKey((a,b)->b+1)
+                .foreach(result -> System.out.println(result._1()+","+result._2()));
+
+
+    }
+
+}
